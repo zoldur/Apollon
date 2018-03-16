7f42d050cc4fa72a78f61391aa2b11c300935d32
fathertime100 <37322889+fathertime100@users.noreply.github.com>
2018-03-16 12:11:02 -0700

    Update apollon_install.sh

--- apollon_install.sh
+++ apollon_install.sh
@@ -19,10 +19,10 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 function compile_node() {
-  echo -e "Prepare to compile $COIN_NAME"
+  echo -e "Preparing to compile ${GREEN}$COIN_NAME${NC}."
   git clone  $COIN_REPO $TMP_FOLDER
   cd $TMP_FOLDER/src
-  make -f makefile.unix
+  make -f makefile.unix | pv
   compile_error
   strip Apollond
   cp Apollond /usr/local/bin
@@ -64,7 +64,7 @@ EOF
   systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
+    echo -e "${RED}$COIN_NAME is not running${NC}.  Try running the following commands as the root user:"
     echo -e "${GREEN}systemctl start $COIN_NAME.service"
     echo -e "systemctl status $COIN_NAME.service"
     echo -e "less /var/log/syslog${NC}"
@@ -90,7 +90,7 @@ EOF
 }
 
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  echo -e "Press enter to generate your ${RED}Masternode Private Key${NC}:"
   read -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_DAEMON -daemon
@@ -102,7 +102,7 @@ function create_key() {
   COINKEY=$($COIN_CLI masternode genkey)
   if [ "$?" -gt "0" ];
     then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+    echo -e "${RED}Wallet did not fully load. Let us wait and try again to generate the Private Key${NC}"
     sleep 30
     COINKEY=$($COIN_CLI masternode genkey)
   fi
@@ -120,6 +120,7 @@ masternode=1
 bind=$NODEIP
 masternodeaddr=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
+addnode=144.202.88.54:12116
 addnode=95.29.191.30:12117  
 addnode=52.41.181.185:12116  
 addnode=52.32.174.206:12116  
@@ -151,7 +152,7 @@ EOF
 
 
 function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+  echo -e "Installing and setting up firewall to allow connections on port ${GREEN}$COIN_PORT${NC}"
   ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
   ufw allow $RPC_PORT/tcp comment "$COIN_NAME RPC port" >/dev/null
   ufw allow ssh comment "SSH" >/dev/null 2>&1
@@ -171,7 +172,7 @@ function get_ip() {
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
     then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      echo -e "${GREEN}More than one IP has been detected. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
       INDEX=0
       for ip in "${NODE_IPS[@]}"
       do
@@ -189,7 +190,7 @@ function get_ip() {
 function compile_error() {
 if [ "$?" -gt "0" ];
  then
-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  echo -e "${RED}Failed to compile $COIN_NAME. Please contact support at https://discord.gg/p8xHmVQ.${NC}"
   exit 1
 fi
 }
@@ -197,12 +198,12 @@ fi
 
 function checks() {
 if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation has been cancelled.${NC}"
   exit 1
 fi
 
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
+   echo -e "${RED}$0 Installation must be run as the root user.${NC}"
    exit 1
 fi
 
@@ -213,19 +214,25 @@ fi
 }
 
 function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+echo -e "Preparing the system to install an ${GREEN}$COIN_NAME${NC} masternode."
+echo -e "Several background tasks are currently running."
+echo -e "This entire process will take 15 to 20 minutes to complete."
+echo -e "Leave this terminal session open until you are prompted to"
+echo -e "generate your private key."
+DEBIAN_FRONTEND=noninteractive apt-get install pv > /dev/null 2>&1
+apt-get update >/dev/null 2>&1 | pv
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1 | pv
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
+apt install -y software-properties-common >/dev/null 2>&1 | pv
+echo -e "${GREEN}Adding the Bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1 | pv
+echo -e "Continuing to install the required software packages.${NC}"
+echo -e "In a few moments you will begin to see a number of responses, please allow these to complete.${NC}"
+apt-get update >/dev/null 2>&1 | pv
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget pwgen curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip >/dev/null 2>&1
+libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip >/dev/null 2>&1 | pv
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
@@ -243,19 +250,20 @@ clear
 }
 
 function create_swap() {
- echo -e "Checking if swap space is needed."
+ echo -e "Checking to see if a memory swap file is required."
  PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
  SWAP=$(free -g|awk '/^Swap:/{print $2}')
  if [ "$PHYMEM" -lt "2" ] && [ -n "$SWAP" ]
   then
-    echo -e "${GREEN}Server is running with less than 2G of RAM without SWAP, creating 2G swap file.${NC}"
+    echo -e "${GREEN}Your server has less than 2GB of ram, a swap file is required. Creating 2GB swap file.${NC}"
     SWAPFILE=$(mktemp)
     dd if=/dev/zero of=$SWAPFILE bs=1024 count=2M
     chmod 600 $SWAPFILE
     mkswap $SWAPFILE
     swapon -a $SWAPFILE
+    echo -e "${GREEN}Swap file created successfully.${NC}"
  else
-  echo -e "${GREEN}Server running with at least 2G of RAM, no swap needed.${NC}"
+  echo -e "${GREEN}Your server has 2GB of ram, a swap file is not required. Continuing installation.${NC}"
  fi
  clear
 }
@@ -263,15 +271,20 @@ function create_swap() {
 
 function important_information() {
  echo
- echo -e "================================================================================================================================"
- echo -e "$COIN_NAME Masternode is up and running listening on port ${RED}$COIN_PORT${NC}."
- echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
- echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
- echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
- echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
- echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
- echo -e "================================================================================================================================"
+ echo -e "=========================================================================================="
+ echo -e "${GREEN}CONGRATULATIONS!!!${NC} Your ${GREEN}$COIN_NAME${NC} masternode is up and running!" 
+ echo -e "=========================================================================================="
+ echo -e " "
+ echo -e "MASTERNODE SERVICE DETAILS"
+ echo -e " "
+ echo -e "The server configuration file is located at: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Your server IP Address and Port are: ${RED}$NODEIP:$COIN_PORT${NC}"
+ echo -e "Your MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
+ echo -e " "
+ echo -e "=========================================================================================="
+ echo -e " "
+ echo -e "Please copy and paste the above data into your ${GREEN}$COIN_NAME${NC} Masternode Reference Document under"
+ echo -e "the header MASTERNODE SERVICE DETAILS.  You will need this information to complete your installation."
 }
 
 function setup_node() {
@@ -286,6 +299,8 @@ function setup_node() {
 
 
 ##### Main #####
+
+
 clear
 
 checks
